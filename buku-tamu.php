c714fb237174bc754e8fbcc9fc1f378e4ff01353
= <=>
2025-09-11 08:23:11 +0700

    menambahkan halaman buku tamu dan fitur menampilkan data tamu

--- buku-tamu.php
+++ buku-tamu.php
@@ -11,7 +11,13 @@ include_once('template/header.php');
   <!-- DataTales Example -->
   <div class="card shadow mb-4">
     <div class="card-header py-3">
-      <h6 class="m-0 font-weight-bold text-primary">Daftar Tamu</h6>
+      <button type="button" class="btn btn-primary btn-icon-split"
+        data-toggle="modal" data-target="#tambahModal">
+        <span class="icon text-white-50">
+          <i class="fas fa-plus"></i>
+        </span>
+        <span class="text">Data Tamu</span>
+      </button>
     </div>
     <div class="card-body">
       <div class="table-responsive">
@@ -59,6 +65,75 @@ include_once('template/header.php');
 
 </div>
 <!-- /.container-fluid -->
+
+<!-- Modal Tambah -->
+<div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h5 class="modal-title" id="tambahModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <div class="modal-body">
+        <form method="post" action="">
+          <input type="hidden" name="id_tamu" id="id_tamu" value="<?= $kodeTamu ?>">
+
+          <div class="form-group row">
+            <label for="nama_tamu" class="col-sm-3 col-form-label">Nama Tamu</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="nama_tamu" name="nama_tamu">
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="alamat" class="col-sm-3 col-form-label">Alamat</label>
+            <div class="col-sm-8">
+              <textarea class="form-control" id="alamat" name="alamat"></textarea>
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="no_hp" class="col-sm-3 col-form-label">No. Telepon</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="no_hp" name="no_hp">
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="bertemu" class="col-sm-3 col-form-label">Bertemu dg.</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="bertemu" name="bertemu">
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="kepentingan" class="col-sm-3 col-form-label">Kepentingan</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="kepentingan" name="kepentingan">
+            </div>
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+            <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+          </div>
+        </form>
+      </div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
 
 <?php
 include('template/footer.php');
